49e94336c319e927e78568deb3def949d91e4551
spf.eth <sbsends@gmail.com>
2021-10-13 11:29:30 -0400

    all: adding more information to the signatures

--- generated/2021-10-12/typescript/primitives/signature.ts
+++ generated/2021-10-12/typescript/primitives/signature.ts
@@ -7,42 +7,55 @@ export const protobufPackage = "metadata";
 
 export interface signature {
   /** crypto address used to attest to a given address */
-  financialPublicKey: address | undefined;
-  /** unix time */
-  ed25519ExpirationTime: number;
-  /** the temporary signing key */
-  ed25519PublicKey: string;
-  /** signature of ed25519_public_key + ed25519_expiration_time */
-  ed25519Attestation: string;
-  /** signed by the ed25519 tmp key */
+  attestingAddress: address | undefined;
+  /** this gets signed */
+  claims: claims | undefined;
+  /** type of algorithm used */
+  header: header | undefined;
+  /** signature of header by the attesting_public_key */
+  attestation: string;
+  /** signed by the temporary key */
   metadataSignature: string;
 }
 
-const basesignature: object = {
-  ed25519ExpirationTime: 0,
-  ed25519PublicKey: "",
-  ed25519Attestation: "",
-  metadataSignature: "",
-};
+export interface header {
+  /** ed25519 */
+  algorithm: string;
+  /** base64URL */
+  encoding: string;
+}
+
+export interface claims {
+  /** GLASS.XYZ */
+  issuer: string;
+  /** the */
+  temporaryKey: string;
+  /** expires at */
+  expiresAt: string;
+  /** issued at */
+  issuedAt: string;
+}
+
+const basesignature: object = { attestation: "", metadataSignature: "" };
 
 export const signature = {
   encode(
     message: signature,
     writer: _m0.Writer = _m0.Writer.create()
   ): _m0.Writer {
-    if (message.financialPublicKey !== undefined) {
+    if (message.attestingAddress !== undefined) {
       address
-        .encode(message.financialPublicKey, writer.uint32(10).fork())
+        .encode(message.attestingAddress, writer.uint32(10).fork())
         .ldelim();
     }
-    if (message.ed25519ExpirationTime !== 0) {
-      writer.uint32(16).int64(message.ed25519ExpirationTime);
+    if (message.claims !== undefined) {
+      claims.encode(message.claims, writer.uint32(18).fork()).ldelim();
     }
-    if (message.ed25519PublicKey !== "") {
-      writer.uint32(26).string(message.ed25519PublicKey);
+    if (message.header !== undefined) {
+      header.encode(message.header, writer.uint32(26).fork()).ldelim();
     }
-    if (message.ed25519Attestation !== "") {
-      writer.uint32(34).string(message.ed25519Attestation);
+    if (message.attestation !== "") {
+      writer.uint32(34).string(message.attestation);
     }
     if (message.metadataSignature !== "") {
       writer.uint32(42).string(message.metadataSignature);
@@ -58,16 +71,16 @@ export const signature = {
       const tag = reader.uint32();
       switch (tag >>> 3) {
         case 1:
-          message.financialPublicKey = address.decode(reader, reader.uint32());
+          message.attestingAddress = address.decode(reader, reader.uint32());
           break;
         case 2:
-          message.ed25519ExpirationTime = longToNumber(reader.int64() as Long);
+          message.claims = claims.decode(reader, reader.uint32());
           break;
         case 3:
-          message.ed25519PublicKey = reader.string();
+          message.header = header.decode(reader, reader.uint32());
           break;
         case 4:
-          message.ed25519Attestation = reader.string();
+          message.attestation = reader.string();
           break;
         case 5:
           message.metadataSignature = reader.string();
@@ -83,36 +96,27 @@ export const signature = {
   fromJSON(object: any): signature {
     const message = { ...basesignature } as signature;
     if (
-      object.financialPublicKey !== undefined &&
-      object.financialPublicKey !== null
+      object.attestingAddress !== undefined &&
+      object.attestingAddress !== null
     ) {
-      message.financialPublicKey = address.fromJSON(object.financialPublicKey);
+      message.attestingAddress = address.fromJSON(object.attestingAddress);
     } else {
-      message.financialPublicKey = undefined;
+      message.attestingAddress = undefined;
     }
-    if (
-      object.ed25519ExpirationTime !== undefined &&
-      object.ed25519ExpirationTime !== null
-    ) {
-      message.ed25519ExpirationTime = Number(object.ed25519ExpirationTime);
+    if (object.claims !== undefined && object.claims !== null) {
+      message.claims = claims.fromJSON(object.claims);
     } else {
-      message.ed25519ExpirationTime = 0;
+      message.claims = undefined;
     }
-    if (
-      object.ed25519PublicKey !== undefined &&
-      object.ed25519PublicKey !== null
-    ) {
-      message.ed25519PublicKey = String(object.ed25519PublicKey);
+    if (object.header !== undefined && object.header !== null) {
+      message.header = header.fromJSON(object.header);
     } else {
-      message.ed25519PublicKey = "";
+      message.header = undefined;
     }
-    if (
-      object.ed25519Attestation !== undefined &&
-      object.ed25519Attestation !== null
-    ) {
-      message.ed25519Attestation = String(object.ed25519Attestation);
+    if (object.attestation !== undefined && object.attestation !== null) {
+      message.attestation = String(object.attestation);
     } else {
-      message.ed25519Attestation = "";
+      message.attestation = "";
     }
     if (
       object.metadataSignature !== undefined &&
@@ -127,16 +131,16 @@ export const signature = {
 
   toJSON(message: signature): unknown {
     const obj: any = {};
-    message.financialPublicKey !== undefined &&
-      (obj.financialPublicKey = message.financialPublicKey
-        ? address.toJSON(message.financialPublicKey)
+    message.attestingAddress !== undefined &&
+      (obj.attestingAddress = message.attestingAddress
+        ? address.toJSON(message.attestingAddress)
         : undefined);
-    message.ed25519ExpirationTime !== undefined &&
-      (obj.ed25519ExpirationTime = message.ed25519ExpirationTime);
-    message.ed25519PublicKey !== undefined &&
-      (obj.ed25519PublicKey = message.ed25519PublicKey);
-    message.ed25519Attestation !== undefined &&
-      (obj.ed25519Attestation = message.ed25519Attestation);
+    message.claims !== undefined &&
+      (obj.claims = message.claims ? claims.toJSON(message.claims) : undefined);
+    message.header !== undefined &&
+      (obj.header = message.header ? header.toJSON(message.header) : undefined);
+    message.attestation !== undefined &&
+      (obj.attestation = message.attestation);
     message.metadataSignature !== undefined &&
       (obj.metadataSignature = message.metadataSignature);
     return obj;
@@ -145,38 +149,27 @@ export const signature = {
   fromPartial(object: DeepPartial<signature>): signature {
     const message = { ...basesignature } as signature;
     if (
-      object.financialPublicKey !== undefined &&
-      object.financialPublicKey !== null
+      object.attestingAddress !== undefined &&
+      object.attestingAddress !== null
     ) {
-      message.financialPublicKey = address.fromPartial(
-        object.financialPublicKey
-      );
+      message.attestingAddress = address.fromPartial(object.attestingAddress);
     } else {
-      message.financialPublicKey = undefined;
+      message.attestingAddress = undefined;
     }
-    if (
-      object.ed25519ExpirationTime !== undefined &&
-      object.ed25519ExpirationTime !== null
-    ) {
-      message.ed25519ExpirationTime = object.ed25519ExpirationTime;
+    if (object.claims !== undefined && object.claims !== null) {
+      message.claims = claims.fromPartial(object.claims);
     } else {
-      message.ed25519ExpirationTime = 0;
+      message.claims = undefined;
     }
-    if (
-      object.ed25519PublicKey !== undefined &&
-      object.ed25519PublicKey !== null
-    ) {
-      message.ed25519PublicKey = object.ed25519PublicKey;
+    if (object.header !== undefined && object.header !== null) {
+      message.header = header.fromPartial(object.header);
     } else {
-      message.ed25519PublicKey = "";
+      message.header = undefined;
     }
-    if (
-      object.ed25519Attestation !== undefined &&
-      object.ed25519Attestation !== null
-    ) {
-      message.ed25519Attestation = object.ed25519Attestation;
+    if (object.attestation !== undefined && object.attestation !== null) {
+      message.attestation = object.attestation;
     } else {
-      message.ed25519Attestation = "";
+      message.attestation = "";
     }
     if (
       object.metadataSignature !== undefined &&
@@ -190,15 +183,195 @@ export const signature = {
   },
 };
 
-declare var self: any | undefined;
-declare var window: any | undefined;
-var globalThis: any = (() => {
-  if (typeof globalThis !== "undefined") return globalThis;
-  if (typeof self !== "undefined") return self;
-  if (typeof window !== "undefined") return window;
-  if (typeof global !== "undefined") return global;
-  throw "Unable to locate global object";
-})();
+const baseheader: object = { algorithm: "", encoding: "" };
+
+export const header = {
+  encode(
+    message: header,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.algorithm !== "") {
+      writer.uint32(10).string(message.algorithm);
+    }
+    if (message.encoding !== "") {
+      writer.uint32(18).string(message.encoding);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): header {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseheader } as header;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.algorithm = reader.string();
+          break;
+        case 2:
+          message.encoding = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): header {
+    const message = { ...baseheader } as header;
+    if (object.algorithm !== undefined && object.algorithm !== null) {
+      message.algorithm = String(object.algorithm);
+    } else {
+      message.algorithm = "";
+    }
+    if (object.encoding !== undefined && object.encoding !== null) {
+      message.encoding = String(object.encoding);
+    } else {
+      message.encoding = "";
+    }
+    return message;
+  },
+
+  toJSON(message: header): unknown {
+    const obj: any = {};
+    message.algorithm !== undefined && (obj.algorithm = message.algorithm);
+    message.encoding !== undefined && (obj.encoding = message.encoding);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<header>): header {
+    const message = { ...baseheader } as header;
+    if (object.algorithm !== undefined && object.algorithm !== null) {
+      message.algorithm = object.algorithm;
+    } else {
+      message.algorithm = "";
+    }
+    if (object.encoding !== undefined && object.encoding !== null) {
+      message.encoding = object.encoding;
+    } else {
+      message.encoding = "";
+    }
+    return message;
+  },
+};
+
+const baseclaims: object = {
+  issuer: "",
+  temporaryKey: "",
+  expiresAt: "",
+  issuedAt: "",
+};
+
+export const claims = {
+  encode(
+    message: claims,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.issuer !== "") {
+      writer.uint32(10).string(message.issuer);
+    }
+    if (message.temporaryKey !== "") {
+      writer.uint32(18).string(message.temporaryKey);
+    }
+    if (message.expiresAt !== "") {
+      writer.uint32(26).string(message.expiresAt);
+    }
+    if (message.issuedAt !== "") {
+      writer.uint32(34).string(message.issuedAt);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): claims {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseclaims } as claims;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.issuer = reader.string();
+          break;
+        case 2:
+          message.temporaryKey = reader.string();
+          break;
+        case 3:
+          message.expiresAt = reader.string();
+          break;
+        case 4:
+          message.issuedAt = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): claims {
+    const message = { ...baseclaims } as claims;
+    if (object.issuer !== undefined && object.issuer !== null) {
+      message.issuer = String(object.issuer);
+    } else {
+      message.issuer = "";
+    }
+    if (object.temporaryKey !== undefined && object.temporaryKey !== null) {
+      message.temporaryKey = String(object.temporaryKey);
+    } else {
+      message.temporaryKey = "";
+    }
+    if (object.expiresAt !== undefined && object.expiresAt !== null) {
+      message.expiresAt = String(object.expiresAt);
+    } else {
+      message.expiresAt = "";
+    }
+    if (object.issuedAt !== undefined && object.issuedAt !== null) {
+      message.issuedAt = String(object.issuedAt);
+    } else {
+      message.issuedAt = "";
+    }
+    return message;
+  },
+
+  toJSON(message: claims): unknown {
+    const obj: any = {};
+    message.issuer !== undefined && (obj.issuer = message.issuer);
+    message.temporaryKey !== undefined &&
+      (obj.temporaryKey = message.temporaryKey);
+    message.expiresAt !== undefined && (obj.expiresAt = message.expiresAt);
+    message.issuedAt !== undefined && (obj.issuedAt = message.issuedAt);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<claims>): claims {
+    const message = { ...baseclaims } as claims;
+    if (object.issuer !== undefined && object.issuer !== null) {
+      message.issuer = object.issuer;
+    } else {
+      message.issuer = "";
+    }
+    if (object.temporaryKey !== undefined && object.temporaryKey !== null) {
+      message.temporaryKey = object.temporaryKey;
+    } else {
+      message.temporaryKey = "";
+    }
+    if (object.expiresAt !== undefined && object.expiresAt !== null) {
+      message.expiresAt = object.expiresAt;
+    } else {
+      message.expiresAt = "";
+    }
+    if (object.issuedAt !== undefined && object.issuedAt !== null) {
+      message.issuedAt = object.issuedAt;
+    } else {
+      message.issuedAt = "";
+    }
+    return message;
+  },
+};
 
 type Builtin =
   | Date
@@ -217,13 +390,6 @@ export type DeepPartial<T> = T extends Builtin
   : T extends {}
   ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
-
-function longToNumber(long: Long): number {
-  if (long.gt(Number.MAX_SAFE_INTEGER)) {
-    throw new globalThis.Error("Value is larger than Number.MAX_SAFE_INTEGER");
-  }
-  return long.toNumber();
-}
 
 if (_m0.util.Long !== Long) {
   _m0.util.Long = Long as any;
