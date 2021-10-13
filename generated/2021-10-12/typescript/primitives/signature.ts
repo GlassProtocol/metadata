1d261a056b0d9c74f91219481cbe396a2b0ac7b9
spf.eth <sbsends@gmail.com>
2021-10-13 13:48:30 -0400

    all: changing signature types to an enum (explicit)

--- generated/2021-10-12/typescript/primitives/signature.ts
+++ generated/2021-10-12/typescript/primitives/signature.ts
@@ -5,9 +5,73 @@ import { address } from "../primitives/address";
 
 export const protobufPackage = "metadata";
 
+export enum algorithm {
+  ALGORITHM_UNDEFINED = 0,
+  ED25519 = 1,
+  UNRECOGNIZED = -1,
+}
+
+export function algorithmFromJSON(object: any): algorithm {
+  switch (object) {
+    case 0:
+    case "ALGORITHM_UNDEFINED":
+      return algorithm.ALGORITHM_UNDEFINED;
+    case 1:
+    case "ED25519":
+      return algorithm.ED25519;
+    case -1:
+    case "UNRECOGNIZED":
+    default:
+      return algorithm.UNRECOGNIZED;
+  }
+}
+
+export function algorithmToJSON(object: algorithm): string {
+  switch (object) {
+    case algorithm.ALGORITHM_UNDEFINED:
+      return "ALGORITHM_UNDEFINED";
+    case algorithm.ED25519:
+      return "ED25519";
+    default:
+      return "UNKNOWN";
+  }
+}
+
+export enum encoding {
+  ENCODING_UNDEFINED = 0,
+  BASE64_URL = 1,
+  UNRECOGNIZED = -1,
+}
+
+export function encodingFromJSON(object: any): encoding {
+  switch (object) {
+    case 0:
+    case "ENCODING_UNDEFINED":
+      return encoding.ENCODING_UNDEFINED;
+    case 1:
+    case "BASE64_URL":
+      return encoding.BASE64_URL;
+    case -1:
+    case "UNRECOGNIZED":
+    default:
+      return encoding.UNRECOGNIZED;
+  }
+}
+
+export function encodingToJSON(object: encoding): string {
+  switch (object) {
+    case encoding.ENCODING_UNDEFINED:
+      return "ENCODING_UNDEFINED";
+    case encoding.BASE64_URL:
+      return "BASE64_URL";
+    default:
+      return "UNKNOWN";
+  }
+}
+
 export interface signature {
   /** crypto address used to attest to a given address */
-  claimSigningAddress: address | undefined;
+  claimsSigningAddress: address | undefined;
   /** this gets signed */
   claims: claims | undefined;
   /** type of algorithm used */
@@ -20,9 +84,9 @@ export interface signature {
 
 export interface header {
   /** ed25519 */
-  algorithm: string;
+  algorithm: algorithm;
   /** base64URL */
-  encoding: string;
+  encoding: encoding;
 }
 
 export interface claims {
@@ -43,9 +107,9 @@ export const signature = {
     message: signature,
     writer: _m0.Writer = _m0.Writer.create()
   ): _m0.Writer {
-    if (message.claimSigningAddress !== undefined) {
+    if (message.claimsSigningAddress !== undefined) {
       address
-        .encode(message.claimSigningAddress, writer.uint32(10).fork())
+        .encode(message.claimsSigningAddress, writer.uint32(10).fork())
         .ldelim();
     }
     if (message.claims !== undefined) {
@@ -71,7 +135,10 @@ export const signature = {
       const tag = reader.uint32();
       switch (tag >>> 3) {
         case 1:
-          message.claimSigningAddress = address.decode(reader, reader.uint32());
+          message.claimsSigningAddress = address.decode(
+            reader,
+            reader.uint32()
+          );
           break;
         case 2:
           message.claims = claims.decode(reader, reader.uint32());
@@ -96,14 +163,14 @@ export const signature = {
   fromJSON(object: any): signature {
     const message = { ...basesignature } as signature;
     if (
-      object.claimSigningAddress !== undefined &&
-      object.claimSigningAddress !== null
+      object.claimsSigningAddress !== undefined &&
+      object.claimsSigningAddress !== null
     ) {
-      message.claimSigningAddress = address.fromJSON(
-        object.claimSigningAddress
+      message.claimsSigningAddress = address.fromJSON(
+        object.claimsSigningAddress
       );
     } else {
-      message.claimSigningAddress = undefined;
+      message.claimsSigningAddress = undefined;
     }
     if (object.claims !== undefined && object.claims !== null) {
       message.claims = claims.fromJSON(object.claims);
@@ -136,9 +203,9 @@ export const signature = {
 
   toJSON(message: signature): unknown {
     const obj: any = {};
-    message.claimSigningAddress !== undefined &&
-      (obj.claimSigningAddress = message.claimSigningAddress
-        ? address.toJSON(message.claimSigningAddress)
+    message.claimsSigningAddress !== undefined &&
+      (obj.claimsSigningAddress = message.claimsSigningAddress
+        ? address.toJSON(message.claimsSigningAddress)
         : undefined);
     message.claims !== undefined &&
       (obj.claims = message.claims ? claims.toJSON(message.claims) : undefined);
@@ -154,14 +221,14 @@ export const signature = {
   fromPartial(object: DeepPartial<signature>): signature {
     const message = { ...basesignature } as signature;
     if (
-      object.claimSigningAddress !== undefined &&
-      object.claimSigningAddress !== null
+      object.claimsSigningAddress !== undefined &&
+      object.claimsSigningAddress !== null
     ) {
-      message.claimSigningAddress = address.fromPartial(
-        object.claimSigningAddress
+      message.claimsSigningAddress = address.fromPartial(
+        object.claimsSigningAddress
       );
     } else {
-      message.claimSigningAddress = undefined;
+      message.claimsSigningAddress = undefined;
     }
     if (object.claims !== undefined && object.claims !== null) {
       message.claims = claims.fromPartial(object.claims);
@@ -193,18 +260,18 @@ export const signature = {
   },
 };
 
-const baseheader: object = { algorithm: "", encoding: "" };
+const baseheader: object = { algorithm: 0, encoding: 0 };
 
 export const header = {
   encode(
     message: header,
     writer: _m0.Writer = _m0.Writer.create()
   ): _m0.Writer {
-    if (message.algorithm !== "") {
-      writer.uint32(10).string(message.algorithm);
+    if (message.algorithm !== 0) {
+      writer.uint32(8).int32(message.algorithm);
     }
-    if (message.encoding !== "") {
-      writer.uint32(18).string(message.encoding);
+    if (message.encoding !== 0) {
+      writer.uint32(16).int32(message.encoding);
     }
     return writer;
   },
@@ -217,10 +284,10 @@ export const header = {
       const tag = reader.uint32();
       switch (tag >>> 3) {
         case 1:
-          message.algorithm = reader.string();
+          message.algorithm = reader.int32() as any;
           break;
         case 2:
-          message.encoding = reader.string();
+          message.encoding = reader.int32() as any;
           break;
         default:
           reader.skipType(tag & 7);
@@ -233,22 +300,24 @@ export const header = {
   fromJSON(object: any): header {
     const message = { ...baseheader } as header;
     if (object.algorithm !== undefined && object.algorithm !== null) {
-      message.algorithm = String(object.algorithm);
+      message.algorithm = algorithmFromJSON(object.algorithm);
     } else {
-      message.algorithm = "";
+      message.algorithm = 0;
     }
     if (object.encoding !== undefined && object.encoding !== null) {
-      message.encoding = String(object.encoding);
+      message.encoding = encodingFromJSON(object.encoding);
     } else {
-      message.encoding = "";
+      message.encoding = 0;
     }
     return message;
   },
 
   toJSON(message: header): unknown {
     const obj: any = {};
-    message.algorithm !== undefined && (obj.algorithm = message.algorithm);
-    message.encoding !== undefined && (obj.encoding = message.encoding);
+    message.algorithm !== undefined &&
+      (obj.algorithm = algorithmToJSON(message.algorithm));
+    message.encoding !== undefined &&
+      (obj.encoding = encodingToJSON(message.encoding));
     return obj;
   },
 
@@ -257,12 +326,12 @@ export const header = {
     if (object.algorithm !== undefined && object.algorithm !== null) {
       message.algorithm = object.algorithm;
     } else {
-      message.algorithm = "";
+      message.algorithm = 0;
     }
     if (object.encoding !== undefined && object.encoding !== null) {
       message.encoding = object.encoding;
     } else {
-      message.encoding = "";
+      message.encoding = 0;
     }
     return message;
   },
