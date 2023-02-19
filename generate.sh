0fcea0b54b30c7cc06a916171c868e6689e01001
Sam Sends <sbsends@gmail.com>
2023-02-19 12:10:42 -0800

    🔥  removing everything unnneccesary for the metadata

--- generate.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p generated/$1/typescript
-mkdir -p generated/$1/go
-
-cd protos/$1
-
-protoc --go_out=../../generated/$1/go */* 
-
-protoc --plugin=../../node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=../../generated/$1/typescript */* --ts_proto_opt=esModuleInterop=true  --ts_proto_opt=snakeToCamel=false
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/GlassProtocol/metadata
-
-go 1.17
-
-require google.golang.org/protobuf v1.27.1
